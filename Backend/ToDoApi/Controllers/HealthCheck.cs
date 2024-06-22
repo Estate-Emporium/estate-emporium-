99a7558af67fb2080361f222c3212e6cf5d266b5
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-06-22 12:53:37 +0200

    Test API
    Added folders for general structure
    gitignore is only in bakcned folder currently as it is only for C#, can be moved to main folder once more general gitinore is added
    
    Test api has a swagger page and efcore in memory already set up

---
+++ Backend/ToDoApi/Controllers/HealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApi.Controllers
+{
+    [Route("health")]
+    public class HealthCheck : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return new ObjectResult("Health OK");
+        }
+    }
+}
---
+++ Backend/ToDoApi/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using TodoApi;
+using TodoApi.Models;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
