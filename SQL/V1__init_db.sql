78586debd3a59565f2f216a869a7744ef22c4887
Bryce <bryce.grahn@bbd.co.za>
2024-06-24 20:17:12 +0200

    Init db

---
+++ SQL/V1__init_db.sql
@@ -0,0 +1,5 @@
+IF DB_ID('EstateEmporium') IS NULL
+BEGIN
+    CREATE DATABASE EstateEmporium;
+END
+GO
---
+++ SQL/V2__create_transactions_table.sql
@@ -0,0 +1,7 @@
+USE EstateEmporium;
+GO
+
+CREATE TABLE transactions (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    -- The rest goes here
+);
