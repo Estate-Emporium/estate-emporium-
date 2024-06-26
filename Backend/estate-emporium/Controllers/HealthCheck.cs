3492c97ea600fc07aa0e0ded1bb75f5258858e00
Bryce <bryce.grahn@bbd.co.za>
2024-06-26 23:10:43 +0200

    Fix server deployment

--- Backend/estate-emporium/Controllers/HealthCheck.cs
+++ Backend/estate-emporium/Controllers/HealthCheck.cs
@@ -2,22 +2,22 @@
 
 namespace estate_emporium.Controllers
 {
-    [Route("health")]
-    [ApiExplorerSettings(IgnoreApi = true)] //used to hide from swagger, can be applied to entire controller ot jsut 1 endpoint
-    public class HealthCheck : Controller
-    {
+  [Route("health")]
+  [ApiExplorerSettings(IgnoreApi = true)] //used to hide from swagger, can be applied to entire controller ot jsut 1 endpoint
+  public class HealthCheck : Controller
+  {
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return new JsonResult("Health OK");
-        }
-        [HttpGet]
-        [Route("env")]
-        public IActionResult TestEnv()
-        {
-            var testEnv = System.Environment.GetEnvironmentVariable("ENV_WORKING") ?? "FALSE";
-            return new ObjectResult(testEnv);
-        }
+    [HttpGet]
+    public IActionResult Index()
+    {
+      return new JsonResult("Health OK") { StatusCode = 200 };
     }
+    [HttpGet]
+    [Route("env")]
+    public IActionResult TestEnv()
+    {
+      var testEnv = System.Environment.GetEnvironmentVariable("ENV_WORKING") ?? "FALSE";
+      return new ObjectResult(testEnv);
+    }
+  }
 }
