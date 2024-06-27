0e73f29318651ba3e38899bad82848744e996d2d
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-06-27 17:49:05 +0200

    create tables

---
+++ SQL/V2__create_tables.sql
@@ -0,0 +1,31 @@
+USE EstateEmporium;
+GO
+
+CREATE TABLE [PropertySales] (
+  [SaleID] bigint PRIMARY KEY,
+  [BuyerID] bigint NOT NULL,
+  [SellerID] bigint NOT NULL,
+  [PropertyID] bigint NOT NULL,
+  [SalePrice]  bigint NOT NULL,
+  [HomeLoanID] bigint NOT NULL,
+  [PurchaseDate] datetime NOT NULL,
+  [StatusID] integer
+)
+GO
+
+CREATE TABLE [Status] (
+  [StatusID] integer PRIMARY KEY,
+  [Status] nvarchar(255)
+)
+GO
+
+EXEC sp_addextendedproperty
+@name = N'Column_Description',
+@value = 'Pending Failed Sucessful Requested',
+@level0type = N'Schema', @level0name = 'dbo',
+@level1type = N'Table',  @level1name = 'Status',
+@level2type = N'Column', @level2name = 'Status';
+GO
+
+ALTER TABLE [PropertySales] ADD FOREIGN KEY ([StatusID]) REFERENCES [Status] ([StatusID])
+GO
--- SQL/V3__create_accounts_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE accounts (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    account_number CHAR(12) NOT NULL,
-    company_name VARCHAR(50) NOT NULL
-);
