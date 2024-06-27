cc2bdd778253e317b8dd8327c8cb4d7d15d9cee5
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-06-27 18:03:39 +0200

    populate status

---
+++ SQL/V3__Insert_status.sql
@@ -0,0 +1,12 @@
+USE EstateEmporium;
+GO
+
+INSERT INTO [Status] ([StatusID], [Status])
+VALUES (0, 'Not started'),
+       (1, 'Awaiting home loan'),
+       (2, 'Loan approved'),
+       (3, 'Payment received'),
+       (4, 'Ownership transfer complete'),
+	   (5, 'Persona Notified'),
+	   (-1, 'Purchase Falied');
+GO
