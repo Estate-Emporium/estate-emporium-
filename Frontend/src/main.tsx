e508f82b1ca8e2b66c0658900e36dc38447894e8
Sasha Kingwill <sasha-lee.kingwill@bbd.co.za>
2024-07-02 07:12:37 +0200

    KAN-40: Global Styles

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,11 +4,12 @@ import App from "./App";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports.ts";
+import customTheme from "./theme";
 
 Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ChakraProvider>
+  <ChakraProvider theme={customTheme}>
     <App />
   </ChakraProvider>
 );
