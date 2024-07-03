1991d07a6753177decc7a8b154b01ea079e4bb77
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-07-03 15:31:04 +0200

    swagger

--- Backend/estate-emporium/Utils/constsUtils.cs
+++ Backend/estate-emporium/Utils/constsUtils.cs
@@ -2,7 +2,7 @@
 {
     public static class Consts
     {
-        public static readonly int Commission = 6;
+        public static readonly int Commission = 2;
         public static readonly string taxAccount = "central-revenue-service";
         public static readonly string ourAccount = "real-estate-property-sales";
         public static int taxPercentage = 10;
