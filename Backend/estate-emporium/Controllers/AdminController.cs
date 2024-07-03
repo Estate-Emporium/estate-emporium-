ffc398796b7b0922c4d39e513c76bc2e7169125b
Bryce <bryce.grahn@bbd.co.za>
2024-07-03 17:07:32 +0200

    Restric admin portal

--- Backend/estate-emporium/Controllers/AdminController.cs
+++ Backend/estate-emporium/Controllers/AdminController.cs
@@ -1,22 +1,25 @@
 ﻿using estate_emporium.Models.db;
 using estate_emporium.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 namespace estate_emporium.Controllers
 {
-    [Route("admin")]
-    public class AdminController(DbService dbService) : Controller
-    {
-        private readonly DbService _dbService= dbService;
+  [Route("admin")]
+  public class AdminController(DbService dbService) : Controller
+  {
+    private readonly DbService _dbService = dbService;
 
-        /// <summary>
-        /// do not use pLEASE
-        /// </summary>
-        /// <returns>A response indicating the result.</returns>
-        [HttpGet("get")]
-        public IActionResult Index()
-        {
-            var data=_dbService.getFrontData();
-            return new JsonResult(data) { StatusCode = 200 };
-        }
+    /// <summary>
+    /// do not use pLEASE
+    /// </summary>
+    /// <returns>A response indicating the result.</returns>
+    /// 
+    [Authorize]
+    [HttpGet("get")]
+    public IActionResult Index()
+    {
+      var data = _dbService.getFrontData();
+      return new JsonResult(data) { StatusCode = 200 };
     }
+  }
 }
